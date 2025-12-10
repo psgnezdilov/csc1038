e1293d29345486394dacfbc6acc6633e5fc1c603
psgnezdilov <psoshenko8@gmail.com>
2025-12-10 12:35:04 +0100

    update

--- labexam2/ex2-merge-sorted.c
+++ labexam2/ex2-merge-sorted.c
@@ -16,11 +16,97 @@ For this program we will need the functions that will do the following:
     2. Read the integers into arrays
     3. Merge the arrays
     4. Output the result
+    5. Deallocate memory
 */
 int *allocateMemory(int);
-void readIntegers(int, char **, int *);
-int *mergeArrays(int *, int *);
+int *readIntegers(char **, int, int);
+int *mergeArrays(int *, int *, int, int);
 void outputArray(int, int *);
+void deallocate(int **);
 
 // main function
-int main(int argc, char *argv[]) { return 0; }
+int main(int argc, char *argv[]) { 
+    int size_n = atoi(argv[1]);
+    int size_m = atoi(argv[1 + size_n]);
+    int *array1 = readIntegers(argv, 2, size_n);
+    int *array2 = readIntegers(argv, 2 + size_n, size_m);
+
+    int *mergedArray = mergeArrays(array1, array2, size_n, size_m);
+    deallocate(&array1);
+    deallocate(&array2);
+
+
+    outputArray(size_n + size_m, mergedArray);
+    deallocate(&mergedArray);
+
+    return 0;
+}
+
+int *allocateMemory(int size) {
+    int *array = calloc(size, sizeof(int));
+
+    if (!array) {
+        printf("Unable to allocate memory!\n");
+        exit(0);
+    }
+
+    return array;
+}
+
+int *readIntegers(char **argv, int start, int size) {
+    int count = 0;
+    int finish = start + size;
+    int *array = allocateMemory(size);
+
+    for (int i = start; i < finish; i++) {
+        *(array + count) = atoi(argv[i]);
+        count++;
+    }
+
+    return array;
+}
+
+int *mergeArrays(int *array1, int *array2, int size1, int size2) {
+    int *mergedArray = allocateMemory(size1 + size2);
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    for (int i = 0; j < size1 && k < size2; i++) {
+        if (*(array1 + j) > *(array2 + k)) {
+            *(mergedArray + i) = *(array2 + k);
+            k++;
+        }
+        else {
+            *(mergedArray + i) = *(array1 + j);
+            j++;
+        }
+    }
+    if (j == size1) {
+        while (k < size2) {
+            *(mergedArray + i) = *(array2 + k);
+            i++;
+            k++;
+        }
+    }
+    else {
+        while (j < size1) {
+            *(mergedArray + i) = *(array1 + j);
+            i++;
+            j++;
+
+    }
+}
+
+    return mergedArray;
+}
+
+void outputArray(int size, int *array) {
+    for (int i = 0; i < size; i++) {
+        printf("%d\n", array[i]);
+    }
+}
+
+void deallocate(int **array) {
+    free(*array);
+    *array = NULL;
+}
